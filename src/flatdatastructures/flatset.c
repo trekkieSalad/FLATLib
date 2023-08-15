cdc0f119e630b51d86cf46682503203a6aa0e219
trekkieSalad <danigp1912@gmail.com>
2023-08-15 05:27:00 +0200

    VARIOS - Update LICENSE. Añadidas cabeceras con información de licencias a los ficheros

--- src/flatdatastructures/flatset.c
+++ src/flatdatastructures/flatset.c
@@ -1,3 +1,28 @@
+/* flatset.c - Implementation of the FlatSet data structure.
+ *
+ *  FLATLib - Library of formal languages and automata theory.
+ *  Copyright (C) 2023  Daniel José García Paz
+ * 
+ *      SPDX-License-Identifier: LGPL-2.1-or-later
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ *    USA or see <https://www.gnu.org/licenses/>.
+ * 
+ *  Author: Daniel José García Paz <daniel.garcia.paz@udc.es>
+ */
+
 #include <flatset.h>
 #include <flattypes.h>
 #include <datastructfunc.h>
